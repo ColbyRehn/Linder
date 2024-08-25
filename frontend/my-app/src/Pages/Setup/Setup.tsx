0adf2e0ebdc0d37f787cf50b3e7052d87f56b500
ColbyRehn <rehn.colby@gmail.com>
2024-08-25 11:05:57 +1000

    Styles setup pages

--- frontend/my-app/src/Pages/Setup/Setup.tsx
+++ frontend/my-app/src/Pages/Setup/Setup.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
-import './Setup.css'
+import React from "react";
+import "./Setup.css";
 import { useNavigate } from "react-router-dom";
+import logoBig from "../../Assets/tinkedLogoLarge.png";
 
 function Setup() {
   const navigate = useNavigate();
   return (
-    <section id="Interface">
-      <div className='Input'>
-        <p>Name:</p>
-        <input />
+    <section id="SetupInterface">
+      <img
+        src={logoBig}
+        style={{ height: 100, width: 400, objectFit: "contain" }}
+      />
+      <div style={{display: "flex", flexDirection: 'column', gap: 24}}>
+        <div className="Input">
+          <p>Your name?</p>
+          <input className="module-border-wrap" />
+        </div>
+        <div className="Input">
+          <p>Your dream role?</p>
+          <input className="module-border-wrap" />
+        </div>
       </div>
-      <div className='Input'>
-        <p>Role:</p>
-        <input />
-      </div>
-      <button onClick={() => navigate("/photo")}>Next</button>
+      <button id="next" onClick={() => navigate("/photo")}>
+        CONTINUE
+      </button>
     </section>
   );
 }
